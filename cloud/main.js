1a8639d6bd6990113362102a94fb77f2e050edda
LucasG234 <lucasgates234@gmail.com>
2020-07-21 17:34:33 -0500

    Created job to delete old posts

--- cloud/main.js
+++ cloud/main.js
@@ -1,72 +1,25 @@
-// Android push test
-// To be used with:
-// https://github.com/codepath/ParsePushNotificationExample
-// See https://github.com/codepath/ParsePushNotificationExample/blob/master/app/src/main/java/com/test/MyCustomReceiver.java
+Parse.Cloud.job("clean-old", async (request) => {
+    // params: passed in the job call
+    // headers: from the request that triggered the job
+    // log: the ParseServer logger passed in the request
+    // message: a function to update the status message of the job object
+    const { params, headers, log, message } = request;
+    
+    const Post = Parse.Object.extend("Post");
+    const query = new Parse.Query(Post);
+    
+    // date object represents the date one week from current
+    var date = new Date();
+    var dateOffset = (24*60*60*1000) * 7;
+    date.setTime(date.getTime() - dateOffset);
 
-Parse.Cloud.define('pushChannelTest', function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user
-  var params = request.params;
-  var user = request.user;
-
-  var customData = params.customData;
-  var launch = params.launch;
-  var broadcast = params.broadcast;
-
-  // use to custom tweak whatever payload you wish to send
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("deviceType", "android");
-
-  var payload = {};
-
-  if (customData) {
-      payload.customdata = customData;
-  }
-  else if (launch) {
-      payload.launch = launch;
-  }
-  else if (broadcast) {
-      payload.broadcast = broadcast;
-  }
-
-  // Note that useMasterKey is necessary for Push notifications to succeed.
-
-  Parse.Push.send({
-  where: pushQuery,      // for sending to a specific channel
-  data: payload,
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('success');
-});
-
-// iOS push testing
-Parse.Cloud.define("iosPushTest", function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
-  var params = request.params;
-  var user = request.user;
-
-  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
-  var messageText = params.text;
-
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
-
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
-    data: {
-      alert: "Message: " + messageText
+    query.lessThan("createdAt", date);
+    const results = await query.find();
+    
+    for (let i = 0; i < results.length; i++) {
+        var post = results[i];
+        post.destroy()
     }
-  }, { success: function() {
-      console.log("#### PUSH OK");
-  }, error: function(error) {
-      console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
 
-  response.success('success');
+    return "Number found: " + results.length;
 });
-
