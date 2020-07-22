a81dbf8c4ac794303f6329b9218410120d318175
LucasG234 <lucasgates234@gmail.com>
2020-07-22 10:32:00 -0500

    Removed code which was not being used for simplicity

--- index.js
+++ index.js
@@ -11,42 +11,11 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var pushConfig = {};
-
-if (process.env.FCM_API_KEY) {
-    pushConfig['android'] = { apiKey: process.env.FCM_API_KEY || ''};
-}
-
-if (process.env.APNS_ENABLE) {
-    pushConfig['ios'] = [
-        {
-            pfx: 'ParsePushDevelopmentCertificate.p12', // P12 file only
-            bundleId: 'beta.codepath.parsetesting',  // change to match bundleId
-            production: false // dev certificate
-        }
-    ]
-}
-
-
-var filesAdapter = null;  // enable Gridstore to be the default
-if (process.env.S3_ENABLE) {
-    var S3Adapter = require('parse-server').S3Adapter;
-
-    filesAdapter = new S3Adapter(
-        process.env.AWS_ACCESS_KEY,
-        process.env.AWS_SECRET_ACCESS_KEY,
-        {bucket: process.env.AWS_BUCKET_NAME, bucketPrefix: "", directAccess: true}
-    );
-}
-
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '123', //Add your master key here. Keep it secret!
-  push: pushConfig,
-  filesAdapter: filesAdapter,
-  liveQuery: { classNames: ["Message"]},
+  databaseURI: 'mongodb://localhost:27017/dev',
+  cloud: __dirname + '/cloud/main.js',
+  appId: 'myAppId',
+  masterKey: '123', //Add your master key here. Keep it secret!
   publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse'  // needed for Parse Cloud and push notifications
 });
@@ -68,7 +37,7 @@ app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
-var port = process.env.PORT || 4040;
+var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
